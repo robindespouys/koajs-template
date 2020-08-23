939b303e09d4247d0ede2a9fdb954adfddb57245
robin despouys <robin.despouys@gmail.com>
2020-08-23 15:56:43 +0200

    Add a generic utils

---
+++ src/utils/generic-utils.ts
@@ -0,0 +1,22 @@
+export class Utils {
+
+  public static checkFields(fields: any[], body: any): any {
+    let status: number = 200;
+    const missingFields: any[] = [];
+    fields.forEach(name => {
+      if (body[name] == null) {
+        status = 400;
+        missingFields.push(name);
+      }
+    });
+    if (status === 200) {
+      return { status: 200 };
+    }
+    const message: string = 'Missing field' + (missingFields.length > 1 ? 's : ' : ' : ') + missingFields.toString();
+    return {
+      status,
+      body: message,
+    };
+  }
+
+}
