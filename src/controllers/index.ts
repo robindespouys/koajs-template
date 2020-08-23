49258ee5b4eddb049ce8664c361a74b2f271d8b5
robin despouys <robin.despouys@gmail.com>
2020-08-23 15:58:04 +0200

    Update the routes and controllers index file

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,3 +1,5 @@
 // controllers/index.ts
 
 export { UserController as user } from './user';
+export { ThingController as thing} from './thing';
+export { AuthController as auth} from './auth';
--- src/routes/rest-routes.ts
+++ src/routes/rest-routes.ts
@@ -1,12 +1,23 @@
 // routes/rest-routes.ts
 import * as Router from 'koa-router';
 import * as controller from './../controllers';
+import isAuth from './../middlewares/isAuth';
 
 export const restRouter = new Router();
 
 // Routes for the user entity
 restRouter.get('/users', controller.user.getAllUsers);
 restRouter.get('/users/:id', controller.user.getUser);
-restRouter.post('/users', controller.user.createUser);
 restRouter.put('/users/:id', controller.user.updateUser);
-restRouter.delete('/users/:id', controller.user.deleteUser);
+
+// Routes for the authentication
+restRouter.post('/auth/signup', controller.auth.signUp);
+restRouter.post('/auth/signin', controller.auth.signIn);
+restRouter.post('/auth/signout', controller.auth.signOut);
+
+// Routes for the thing entity
+restRouter.get('/things', controller.thing.getAllThings);
+restRouter.get('/things/:id', controller.thing.getThing);
+restRouter.post('/things', controller.thing.createThing);
+restRouter.put('/things/:id', controller.thing.updateThing);
+restRouter.delete('/things/:id', controller.thing.deleteThing);
