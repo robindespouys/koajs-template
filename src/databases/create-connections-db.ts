c45b2a76301cd7a73998c41ef5ad091435fc2e2b
Alexandre Du Lorier <adulorier@gmail.com>
2022-01-04 14:20:12 +0100

    use try catch syntax

--- src/databases/create-connections-db.ts
+++ src/databases/create-connections-db.ts
@@ -10,31 +10,30 @@ import fs from "fs";
 import { tables } from "./tables-db";
 
 export const postgresDB = async () => {
-  return await createConnection({
-    type: "postgres",
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    ssl:
-      process.env.DB_SSL_CA_PATH === ""
-        ? false
-        : {
-            ca: fs.readFileSync(process.env.DB_SSL_CA_PATH).toString(),
-          },
-    entities: tables,
-    logging:
-      process.env.ACTIVATE_LOG_QUERIES === "true"
-        ? ["query", "error"]
-        : ["error"],
-    synchronize: true,
-  })
-    .then((_connection) => {
-      console.log(`Connected to ${process.env.NODE_ENV} Database !`);
-      return _connection;
-    })
-    .catch((error: any) => {
-      console.error(error);
+  try {
+    const connection = await createConnection({
+      type: "postgres",
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      ssl:
+        process.env.DB_SSL_CA_PATH === ""
+          ? false
+          : {
+              ca: fs.readFileSync(process.env.DB_SSL_CA_PATH).toString(),
+            },
+      entities: tables,
+      logging:
+        process.env.ACTIVATE_LOG_QUERIES === "true"
+          ? ["query", "error"]
+          : ["error"],
+      synchronize: true,
     });
+    console.info(`Connected to ${process.env.NODE_ENV} Database !`);
+    return connection;
+  } catch (error) {
+    console.error(error);
+  }
 };
