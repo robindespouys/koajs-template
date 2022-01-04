abb18243d412ff163bc9e1fd267fd6711aa139c8
Alexandre Du Lorier <adulorier@gmail.com>
2022-01-04 13:04:31 +0100

    format with prettier

--- pm2.config.js
+++ pm2.config.js
@@ -1,11 +1,13 @@
 module.exports = {
-    apps: [{
-        name: "koajs-template-prod",
-        script: "./src/server.ts",
-        watch: false,
-        env: {
-            "PORT": 4444,
-            "NODE_ENV": "production"
-        },
-    }]
-}
+  apps: [
+    {
+      name: "koajs-template-prod",
+      script: "./src/server.ts",
+      watch: false,
+      env: {
+        PORT: 4444,
+        NODE_ENV: "production",
+      },
+    },
+  ],
+};
