3fc5aaaaa355a598b4b2580d65a8734e0c95cba7
Alexandre Du Lorier <adulorier@gmail.com>
2022-01-04 14:20:00 +0100

    remove unnecessary class

--- src/server.ts
+++ src/server.ts
@@ -4,39 +4,40 @@ import Koa from "koa";
 import bodyParser from "koa-bodyparser";
 import path from "path";
 import dotenv from "dotenv";
+import { Server } from "http";
+import { Connection } from "typeorm";
 
 import { postgresDB } from "./databases/create-connections-db";
 import isAuth from "./middlewares/isAuth";
-import { restRouter } from "./routes/rest-routes";
+import restRouter from "./routes/rest-routes";
+
+interface Context {
+  dbConnection: Connection;
+  app: Server;
+}
 
 dotenv.config({
   path: path.resolve(__dirname, `../config/${process.env.ENVIRONMENT}.env`),
 });
+console.info(`${process.env.ENVIRONMENT} MODE`);
 
-export class Server {
-  public static async startServer(): Promise<any> {
-    const myApp = new Koa();
-    const dbConnection = await postgresDB();
-    console.log(`${process.env.ENVIRONMENT} MODE`);
+const portNumber = Number(process.env.PORT) || 3333;
 
-    /**
-     * Enable bodyParser for easier body parsing. you can still have fun with ctx.req
-     * if you don't want to use this library
-     */
-    myApp.use(bodyParser());
+export const startServer: () => Promise<Context> = async () => {
+  const myApp = new Koa();
+  const dbConnection = await postgresDB();
 
-    myApp.use(isAuth);
-    /** Tell our application to use the router we created */
-    myApp.use(restRouter.routes());
+  myApp.use(bodyParser());
+  myApp.use(isAuth);
+  myApp.use(restRouter.routes());
 
-    const portNumber = Number(process.env.PORT) || 3333;
-    console.log(`listening on port ${portNumber}`);
+  const app = myApp.listen(portNumber);
+  console.info(`listening on port ${portNumber}`);
 
-    return {
-      dbConnection,
-      app: myApp.listen(portNumber),
-    };
-  }
-}
+  return {
+    dbConnection,
+    app,
+  };
+};
 
-if (process.env.ENVIRONMENT !== "test") Server.startServer();
+if (process.env.ENVIRONMENT !== "test") startServer();
